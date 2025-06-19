5d46542d24858d36f7bfb4cb5b71dc1d3b6818c7
Jk09010 <JADHAVKRUNAL149@GMAIL.COM>
2025-06-19 06:22:30 +0530

    new files

---
+++ Basic/Datatypes.py
@@ -0,0 +1,28 @@
+
+#numerica datatype
+# a = 20
+# b = 12.55
+# c = 2 + 4j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+#sequnce datatype
+
+# a = "Hello"
+# b = [10,20,30,40,50]
+# c = (10,20,30,40,50)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# subjects = {"python":10,"Java":20}
+# print(subjects)
+
+# s = {10,20,30,40,50,50}
+# print(s)
+
+
+b = True
+print(type(b))
