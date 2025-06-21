58cd2d95c032240438e07927322a1eb2980ee7d1
Jk09010 <JADHAVKRUNAL149@GMAIL.COM>
2025-06-21 08:34:48 +0530

    statement

---
+++ Basic/002_statements/conditional.py
@@ -0,0 +1,14 @@
+
+m=60
+if m>90 and m<=100:
+    print("grade a")
+elif m>70 and m<=90:
+    print("grade b")
+elif m>50 and m<=70:
+    print("grade c")
+elif m>35 and m<=50:
+    print("grade d")
+elif m>=0 and m<=35:
+    print("grade f")
+else:
+    print("invalid")
