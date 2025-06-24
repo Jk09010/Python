a283cbb4ce926b66b40aa3d3f08e583cba9965ba
Jk09010 <JADHAVKRUNAL149@GMAIL.COM>
2025-06-24 06:36:35 +0530

    update

--- Basic/002_statements/conditional.py
+++ Basic/002_statements/conditional.py
@@ -1,14 +1,46 @@
+age = 19
+if age>=18 :
+    print("elegible for voting")
+else :
+    print("not eligible for voting")
 
-m=60
-if m>90 and m<=100:
-    print("grade a")
-elif m>70 and m<=90:
-    print("grade b")
-elif m>50 and m<=70:
-    print("grade c")
-elif m>35 and m<=50:
-    print("grade d")
-elif m>=0 and m<=35:
-    print("grade f")
-else:
-    print("invalid")
+# a = 100
+# b = 200
+# c = 300
+
+# )if a>b :
+# #     if a>c :
+# #         print("A is greater")
+# #     else :
+# #         print("C is greater")
+# #  :
+# #     if b>c:
+# #         print("B is greater")
+# #     else :
+# #         print("C is greater")
+
+
+# # if a>b and a>c:
+# #     print("A is greater")
+# # elif b>a and b>c:
+# #     print("B is greater")
+# # elif c>a and c>b:
+# #     print("C is greater")
+# # else:
+# #     print("No result")
+
+
+
+# m=60
+# if m>90 and m<=100:
+#     print("grade a")
+# elif m>70 and m<=90:
+#     print("grade b")
+# elif m>50 and m<=70:
+#     print("grade c")
+# elif m>35 and m<=50:
+#     print("grade d")
+# elif m>=0 and m<=35:
+#     print("grade f")
+# else:
+#     print("invalid")
---
+++ Basic/basic/basicfuntion.py
@@ -0,0 +1,22 @@
+# # print ("hello,world!")
+# # print('Hello, Python')
+# # print('Hello " world')
+# # print ("""hello 
+# # krunal
+# #  wolrd
+# #   pyhton""")
+ 
+# #  a = input("enter your name :")
+# #  print(a)
+#  a = int(input("enter value :"))
+#  print(type(a))
+
+
+a = "hello python i am krunal".split(" ")
+    print(a)
+# #  fname = "krunal"
+# #  lname = "jadhav"
+
+# # #  print ("my fname is {0} and lname is {1}".format(fnmae,lname))
+
+# # print(f"my name is {fname} and lname is {lname}")
